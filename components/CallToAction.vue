b665d05f92214804e2459eb8c08c9d6b8c1b7d25
icat108 <ichacnakval@gmail.com>
2022-04-28 18:10:27 +0700

    This Project

---
+++ components/CallToAction.vue
@@ -0,0 +1,19 @@
+<template>
+  <section class="call-to-action bg-purple-progress pt-64 pb-10">
+    <div class="container mx-auto">
+      <div class="w-full text-center">
+        <h1 class="text-5xl text-white font-semibold">
+          Cara mudah untuk pendanaan
+          <br />
+          dengan ide dan inovasi terbaik
+        </h1>
+        <button
+          @click="$router.push({ path: '/register' })"
+          class="inline-block bg-orange-button hover:bg-green-button text-white font-semibold px-6 py-4 mt-8 text-lg rounded-full"
+        >
+          Getting Started
+        </button>
+      </div>
+    </div>
+  </section>
+</template>
---
+++ components/Footer.vue
@@ -0,0 +1,59 @@
+<template>
+  <footer class="bg-purple-progress py-20 text-white text-lg">
+    <div class="container mx-auto">
+      <div class="sm:flex mb-4">
+        <div class="sm:w-1/2 sm:mt-0 mt-8 h-auto">
+          <img src="/logo-footer.svg" alt="" class="mb-3" />
+          <p class="font-light">
+            Membantu Dan Menjalankan <br />Program - Program terbaik anak bangsa
+          </p>
+        </div>
+        <div class="sm:w-1/1 h-auto">
+          <div class="mb-8 font-bold">Explore</div>
+          <ul class="font-light">
+            <!-- <li class="mb-3">Our Services</li>
+            <li class="mb-3">Equity System</li>
+            <li class="mb-3">Refund</li>
+            <li class="mb-3">Shareholder</li> -->
+            <li class="mb-3"><a href="#projects">Project</a></li>
+            <li class="mb-3"><a href="#features">Features</a></li>
+            <li class="mb-3">
+              <a href="https://www.startupranking.com/top/indonesia"
+                >Top Startups Indonesia</a
+              >
+            </li>
+            <!-- <li class="mb-3"><a href="#testimonials">Testimonials</a></li> -->
+          </ul>
+        </div>
+        <div class="sm:w-1/5 h-auto">
+          <div class="mb-8 font-bold">Investor</div>
+          <ul class="font-light">
+            <!-- <li class="mb-3">Login</li> -->
+            <li class="mb-3">
+              <a href="/login">Login</a>
+            </li>
+            <li class="mb-3">
+              <a href="/register">Register</a>
+            </li>
+
+            <!-- <li class="mb-3">Top Startups</li> -->
+            <li class="mb-3">How-to Tutorials</li>
+            <!-- <li class="mb-3">Withdrawl</li> -->
+          </ul>
+        </div>
+        <div class="sm:w-1/4 h-auto">
+          <div class="mb-8 font-bold">Office</div>
+          <ul class="font-light">
+            <li class="mb-3">+62812 9293 1896</li>
+            <li class="mb-3">
+              Gedung Artha Graha, Jl. Jenderal Sudirman No.Kav 52-53, RT.5/RW.3,
+              Senayan, Kebayoran Baru, South Jakarta City, Jakarta 12190
+            </li>
+            <li class="mb-3">No.12 (Backer)</li>
+            <li class="mb-3">support@backer.id</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
