23a0e9539a3cbe04bf06983a591fe0f28d89a423
Ricad Agus Setiawan <68917848+icat108@users.noreply.github.com>
2022-05-08 07:40:48 -0500

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -60,7 +60,7 @@ export default {
    ** See https://axios.nuxtjs.org/options
    */
   axios: {
-  baseURL  : 'https://627057f96a36d4d62c1804a0.mockapi.io',
+  baseURL  : 'http://localhost:8080',
   },
   /*
    ** Build configuration
