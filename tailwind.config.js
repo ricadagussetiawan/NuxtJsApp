01d0881a4a4117ee37d42a60fca0ef4e62f4a8a8
Ricad Agus Setiawan <68917848+icat108@users.noreply.github.com>
2022-05-09 12:14:21 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -26,7 +26,7 @@ module.exports = {
   plugins: [],
   purge: {
     // Learn more on https://tailwindcss.com/docs/controlling-file-size/#removing-unused-css
-    enabled: process.env.NODE_ENV === 'production',
+    enabled: process.env.NODE_ENV === 'http://localhost:8080',
     content: [
       'components/**/*.vue',
       'layouts/**/*.vue',
