a07614d7ddea36f79aaecdf533dde6bcc70d50b5
Ricad Agus Setiawan <68917848+icat108@users.noreply.github.com>
2022-05-16 21:43:07 +0700

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -61,7 +61,7 @@ export default {
    */
   axios: {
 //     baseURL  : 'https://richstore.my.id',
-//   baseURL  : 'http://103.183.75.93',
+  baseURL  : 'http://103.183.75.93',
 //     baseURL : 'https://627057f96a36d4d62c1804a0.mockapi.io',
     
   },
