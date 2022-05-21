d097c80d19805c56f1aeca8881c6f9a99ead9a22
Ricad Agus Setiawan <68917848+icat108@users.noreply.github.com>
2022-05-21 23:48:41 +0700

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -65,7 +65,7 @@ export default {
 //     baseURL : 'https://627057f96a36d4d62c1804a0.mockapi.io',
 //     baseURL : 'https://103.186.0.49:443',
 //     baseURL : 'https://103.134.154.213:8080',
-    baseURL : 'https://116.193.191.113:8080',
+    baseURL : 'https://ricad-store.my.id',
 //    baseURL : 'https://ricad-store.my.id',
     
   },
