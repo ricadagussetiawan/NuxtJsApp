c010d552fc5000e4e391155b6cf3d9b29706afc8
Ricad Agus Setiawan <68917848+icat108@users.noreply.github.com>
2022-05-26 16:21:38 +0700

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -77,16 +77,16 @@ proxy: true,
       local: {
         endpoints: {
           login: {
-//             url: '/api/v1/sessions',
-            baseURL : 'https://ricad-store.my.id',
+            url: '/api/v1/sessions',
+//             baseURL : 'https://ricad-store.my.id',
             method: 'post',
             propertyName: 'data.token',
           },
           logout: false,
           user: {
-//             url: '/api/v1/users/fetch',
+            url: '/api/v1/users/fetch',
             
-           baseURL : 'https://ricad-store.my.id',
+//            baseURL : 'https://ricad-store.my.id',
             method: 'get',
             propertyName: 'data',
           },
