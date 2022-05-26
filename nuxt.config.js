5dcd3e3a9239e90690386768c7592dc0e888df4a
Ricad Agus Setiawan <68917848+icat108@users.noreply.github.com>
2022-05-26 12:34:09 +0700

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -16,14 +16,14 @@ export default {
    ** See https://nuxtjs.org/api/configuration-head
    */
   head: {
-    title: process.env.npm_package_name || 'https://ricad-store.my.id',
+    title: process.env.npm_package_name || '',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       {
         hid: 'description',
         name: 'description',
-        content: process.env.npm_package_description || 'https://ricad-store.my.id',
+        content: process.env.npm_package_description || '',
       },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
